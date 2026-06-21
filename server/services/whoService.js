d639ee76da7cc3a4173a929f4f7ae96670528983
KJSodipe <kolajosodipe@gmail.com>
2026-06-21 00:38:38 -0500

    fixed data fetching date logic

--- server/services/whoService.js
+++ server/services/whoService.js
@@ -1,9 +1,18 @@
 const axios = require("axios");
 
 async function getOutbreaks() {
-  const response = await axios.get("https://www.who.int/api/news/diseaseoutbreaknews");
-  //console.log(response.data)
-  return response.data.value;
+  const sixMonthsAgo = new Date();
+  sixMonthsAgo.setMonth(sixMonthsAgo.getMonth() - 6);
+  const filterDate = sixMonthsAgo.toISOString();
+
+
+  const response = await axios.get("https://www.who.int/api/news/diseaseoutbreaknews", {
+  params: {
+    "$filter": `PublicationDateAndTime ge ${filterDate}`
+  }
+});
+
+return response.data.value;
 }
 
 module.exports = { getOutbreaks };
