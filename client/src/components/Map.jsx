2c181e2127a7a3256c4669de4c02937a4947df90
KJSodipe <kolajosodipe@gmail.com>
2026-06-20 22:07:15 -0500

    fix leaflet marker icons

--- client/src/components/Map.jsx
+++ client/src/components/Map.jsx
@@ -1,6 +1,16 @@
 import { useState, useEffect } from 'react'
 import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet'
 import 'leaflet/dist/leaflet.css'
+import L from 'leaflet'
+import markerIcon from 'leaflet/dist/images/marker-icon.png'
+import markerShadow from 'leaflet/dist/images/marker-shadow.png'
+
+delete L.Icon.Default.prototype._getIconUrl
+L.Icon.Default.mergeOptions({
+    iconUrl: markerIcon,
+    shadowUrl: markerShadow,
+})
+
 
 function Map() {
     const [outbreaks, setOutbreaks] = useState([])
